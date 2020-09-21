3889ce29af419bfb1741e4b2e1c88bac7d2cb6a5
Gitte <67017479+Gitte-R@users.noreply.github.com>
2020-09-21 22:45:39 +0200

    Add files via upload

--- Opgave StenSaksPapir/jsSkabelon.js
+++ Opgave StenSaksPapir/jsSkabelon.js
@@ -1,32 +1,57 @@
-function User()
-{
+let computerVins = 0;
+let userVins = 0;
+let counts = 0;
+
+function User(){
     let userChoiceNumber = document.getElementsByName("valg");
+    let computerChoice = Computer();
 
     for(i = 0; i < userChoiceNumber.length; i++) {
         if(userChoiceNumber[i].checked) {
             var userChoice = userChoiceNumber[i].value;
+            counts++;
             break; 
         }
     }
-    let computerChoice = Computer();
-    document.getElementById("computerInput").innerHTML = computerChoice;
+    document.getElementById("turns").innerHTML = counts;
 
-    computerChoice === userChoice ? document.getElementById("resultHere").innerHTML = `I har begge valgt ${userChoice}. Prøv igen.`: 
-    Results(computerChoice,userChoice);
+    computerChoice === userChoice ? (document.getElementById("resultHere").innerHTML = 
+    "Uafgjort. I har begge valgt " + userChoice) + 
+    (document.getElementById("computerInput").innerHTML = computerChoice): 
+    userChoice === undefined ? document.getElementById("resultHere").innerHTML =
+    "Hov. Du har ikke valgt noget":
+    Results(computerChoice, userChoice);
 }
 
-function Computer()
-{
+function Computer(){
     let computerChoice = Math.ceil(Math.random()*3);
     computerChoice = computerChoice === 1 ? "Sten": computerChoice = computerChoice === 2 ? "Saks": "Papir";
     return computerChoice;
 }
 
-function Results(computerChoice,userChoice)
-{
+function Results(computerChoice, userChoice){
+    document.getElementById("computerInput").innerHTML = computerChoice;
+
     (userChoice === "Sten" && computerChoice === "Saks") ||
     (userChoice === "Papir" && computerChoice === "Sten") || 
     (userChoice === "Saks" && computerChoice === "Papir") ?
-    document.getElementById("resultHere").innerHTML = "Tillykke. Du vandt":  
-    document.getElementById("resultHere").innerHTML = "Desværre ingen held denne gang."; 
+    (document.getElementById("resultHere").innerHTML = "Tillykke. Ét point til dig") && userVins++:  
+    (document.getElementById("resultHere").innerHTML = "Desværre. Ét point til computeren") && computerVins++; 
+
+    BestOfTree(userVins, computerVins);
+}
+
+function BestOfTree (userVins, computerVins){
+    if (userVins === 2 || computerVins === 2){
+        userVins===2 ? document.write("Tillykke du vandt. Vil du prøve igen "):
+        document.write("Du tabte desværre. Vil du prøve igen? ");
+        CreateBotton();
+    }   
+}
+
+function CreateBotton() {
+    var yesBotton = document.createElement("BUTTON");
+    yesBotton.innerHTML = "JA";
+    document.body.appendChild(yesBotton);
+    yesBotton.addEventListener ("click", function() {location.reload()})
 }
